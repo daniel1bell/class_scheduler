75cd3c83f775feb37ac6c4a8ed4a7c10c014ad69
daniel1bell <daniel1bell@me.com>
2014-04-29 16:02:57 +0100

    Working views for location and courses.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,28 +1,42 @@
 class LocationsController < ApplicationController
+  
+  def index
+    @locations = Location.all  
+  end
+
   def new
+    @location = Location.new
   end
 
   def create
-  end
+    @location = Location.new(params[:location])
 
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  def index
-    @locations = Location.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @locations }
+    if @location.save
+      redirect_to locations_path, notice: 'Location was successfully created.' 
+    else
+      render action: "new" 
     end
   end
 
-  def show
+  def edit
+    @location = Location.find(params[:id])
   end
+
+  def update
+    @location = Location.find(params[:id])
+
+    if @location.update_attributes(params[:location])
+      redirect_to locations_path, notice: 'Location was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+
+    redirect_to locations_path
+  end
+
 end
